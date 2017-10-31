09c0c98096c3f843427103e554c2abd438d55eb4
Lehlohonolo <lehlohonoloisaac25@gmail.com>
2017-10-31 15:31:39 +0200

    Create Pokemon class

---
+++ Pokedex/Pokedex/Model/Pokemon.swift
@@ -0,0 +1,30 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by dvt on 2017/10/31.
+//  Copyright © 2017 dvt. All rights reserved.
+//
+
+import Foundation
+
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self.pokedexId = pokedexId
+    }
+    
+}
